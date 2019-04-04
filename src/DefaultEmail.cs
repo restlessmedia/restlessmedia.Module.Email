e80c1ac5818498fb2c8970a7f28786f2dcfc2a4b
restlessmedia <phillycooper@hotmail.com>
2019-04-04 17:42:16 +0100

    Added default email, fixed feeds.

---
+++ src/DefaultEmail.cs
@@ -0,0 +1,11 @@
+﻿namespace restlessmedia.Module.Email
+{
+  /// <summary>
+  /// Sends an email to and from the default 'from' address
+  /// </summary>
+  public class DefaultEmail : Email
+  {
+    public DefaultEmail(IEmailContext emailContext, string subject = null, string body = null, bool isHtml = false)
+      : base(emailContext.EmailSettings.FromEmail, emailContext.EmailSettings.FromEmail, subject, body, isHtml) { }
+  }
+}
