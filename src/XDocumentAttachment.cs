7d54eb99ca7b2534a5674eeec832f56128f9d769
Philip Cooper <phillycooper@hotmail.com>
2020-06-05 19:55:27 +0100

    Added XDocumentAttachment

---
+++ src/XDocumentAttachment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace restlessmedia.Module.Email
+{
+  public class XDocumentAttachment : EmailAttachment
+  {
+    public XDocumentAttachment(string name, XDocument document)
+      : base(name, "text/xml")
+    {
+      _document = document ?? throw new ArgumentNullException(nameof(document));
+    }
+
+    public override Stream ContentStream
+    {
+      get
+      {
+        Stream stream = new MemoryStream();
+        _document.Save(stream);
+        stream.Position = 0;
+        return stream;
+      }
+    }
+
+    private readonly XDocument _document;
+  }
+}
