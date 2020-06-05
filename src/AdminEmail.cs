18543620487d6e9f24da81b8c09526a1df23fb8c
Philip Cooper <phillycooper@hotmail.com>
2020-06-05 19:43:31 +0100

    Added admin email

---
+++ src/AdminEmail.cs
@@ -0,0 +1,13 @@
+﻿namespace restlessmedia.Module.Email
+{
+  public class AdminEmail : Email
+  {
+    public AdminEmail(IEmailContext emailContext, string subject = null, string body = null, bool isHtml = false)
+      : base(emailContext.EmailSettings.AdminEmail, emailContext.EmailSettings.AdminEmail, subject, body, isHtml)
+    {
+      EmailContext = emailContext;
+    }
+
+    protected readonly IEmailContext EmailContext;
+  }
+}
